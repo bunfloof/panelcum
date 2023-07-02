5fc09582ba0a64450fabb6cbe796fabca7064894
root <root@vpsdev.bunis.gay>
2023-07-02 10:57:47 +0000

    up tailwind and coems subnav

--- resources/scripts/components/elements/SubNavigation.tsx
+++ resources/scripts/components/elements/SubNavigation.tsx
@@ -2,22 +2,32 @@ import styled from 'styled-components/macro';
 import tw, { theme } from 'twin.macro';
 
 const SubNavigation = styled.div`
-    ${tw`w-full bg-neutral-700 shadow overflow-x-auto`};
+    ${tw`w-full h-12 bg-black shadow overflow-x-auto`};
 
     & > div {
-        ${tw`flex items-center text-sm mx-auto px-2`};
+        ${tw`flex items-center text-base mx-auto px-2`};
         max-width: 1200px;
 
         & > a,
         & > div {
-            ${tw`inline-block py-3 px-4 text-neutral-300 no-underline whitespace-nowrap transition-all duration-150`};
+            ${tw`relative inline-flex items-center py-3 px-4 text-neutral-300 no-underline whitespace-nowrap transition-all duration-150 z-10`}; // Add z-10
 
             &:not(:first-of-type) {
-                ${tw`ml-2`};
+                ${tw`ml-0`};
+            }
+
+            &::before {
+                content: '';
+                ${tw`absolute inset-2 bg-transparent rounded-md transition-all duration-150`};
+                z-index: -1; // Acab z-index: -1
             }
 
             &:hover {
                 ${tw`text-neutral-100`};
+
+                &::before {
+                    ${tw`bg-neutral-500`};
+                }
             }
 
             &:active,
